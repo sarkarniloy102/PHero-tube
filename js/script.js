db33c2cf642ca721ce351f6620dc16f1b2efac2b
sarkarniloy102 <sarkarniloy102@gmail.com>
2023-09-02 03:14:16 +0600

    add time

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const LoadChannel = async (id) => {
+const LoadChannel = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const channeldata = await res.json();
     // console.log(channeldata.data[0].category_id);
@@ -14,7 +14,7 @@ const LoadChannel = async (id) => {
     });
 }
 
-const HandleCategories = async (id) => {
+const HandleCategories = async (id = 1000) => {
     const res = await fetch(` https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await (res.json());
     // console.log(data.message);
@@ -40,13 +40,19 @@ const DisplayTubeCart = (channels, ChannelContainer) => {
 
     channels.forEach(channel => {
         // console.log(channel.category_id);
+        const postedDate = channel.others?.posted_date;
+        const timeinhours = Math.floor(postedDate / 3600);
+        const timeinMin = postedDate % 60;
+        console.log(postedDate, timeinhours, timeinMin);
+
         const channelcard = document.createElement('div');
         channelcard.classList = `card  bg-base-100  p-3`;
         channelcard.innerHTML = `
-        <figure>
+        <figure class="relative">
         <img class="w-[100%] h-36" src="${channel.thumbnail}" alt="Shoes" />
         </figure>
-        <div class=" flex gap-5 items-center p-5 pb-0">
+        <p class="absolute text-xs  bg-stone-700 rounded-md text-white px-3 top-[50%] left-[45%]">${timeinhours}  hrs ${' '}${timeinMin}${' '}min ago</p>
+        <div class="  flex gap-5 items-center p-5 pb-0">
         <div>
         <img class="w-10 h-10  rounded-full" src="${channel.authors[0].profile_picture}" alt="Shoes" />
         </div>
@@ -57,9 +63,11 @@ const DisplayTubeCart = (channels, ChannelContainer) => {
        
        <p class="pl-20 text-sm">${channel.authors[0].profile_name}</p>
        <p class="pl-20 text-sm" >${channel.others.views} views</p>
-     
-      
+       
          `;
+
+
+
         ChannelContainer.appendChild(channelcard);
     });
 
